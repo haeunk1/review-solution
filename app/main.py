425451bad06b083cf1c295cd7ecbbba038ee0505
Haeun Kim <53562331+haeunk1@users.noreply.github.com>
2026-03-05 18:07:03 +0900

    auth 임시 주석처리

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from app.api.v1 import auth, reviews, hospitals
+from app.api.v1 import  reviews, hospitals
 
 app = FastAPI(title="Review AI Solution")
 
 # API 라우터 등록
-app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
+#app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
 app.include_router(reviews.router, prefix="/api/v1/reviews", tags=["reviews"])
 app.include_router(hospitals.router, prefix="/api/v1/hospitals", tags=["hospitals"])
 @app.get("/")
